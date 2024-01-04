a817d95ec33ce1e9a6c83882ab1bc6e22f8c554c
tuanson28 <tuanson2810@gmail.com>
2024-01-04 14:50:39 +0700

    sua lan 5

--- canvas.js
+++ canvas.js
@@ -2,7 +2,12 @@
 
     function playMusic(){
       var myAudio = document.getElementById('playAudio');
+        if (myAudio.duration > 0 && !myAudio.paused) {
+            //Its playing...do your job
 
+        } else {
+            myAudio.play()
+        //Not playing...maybe paused, stopped or never played.
       }
     }
 
