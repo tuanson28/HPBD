db10a301d50bd678a043ab7c308cb74d14159fa5
tuanson28 <tuanson2810@gmail.com>
2024-01-04 11:32:21 +0700

    sua lan 2

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em Thư';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100006743765628&mibextid=LQQJ4d';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Tặng Bé Thư';
-const contentCard = 'Chúc Thư có một sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const titleCard = 'Chúc Em Thư';
+const contentCard = 'Chúc Thư có một sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em và chúc mừng năm mới';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
