085a3e45f9e7d38e007cad6583cf21697edfc0dd
tuanson28 <tuanson2810@gmail.com>
2024-01-04 14:30:49 +0700

    sua lan 3

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Thư có một sinh nhật tràn ngập niềm vui v�
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl.jpg';
 const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
