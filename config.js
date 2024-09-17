10c5e1aa6658a83a84046c825cc2499cda7bd289
tuanson28 <151901689+tuanson28@users.noreply.github.com>
2024-09-17 13:18:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Em Thư';
-const giftUrl = 'https://www.facebook.com/profile.php?id=100006743765628&mibextid=LQQJ4d';
+const giftUrl = 'https://drive.google.com/drive/folders/1-0ia2nT7eiT5mNT8Xh1ZI9WWlioseri9';
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Chúc Em Thư';
 const contentCard = 'Chúc Thư có một sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em. <3';
